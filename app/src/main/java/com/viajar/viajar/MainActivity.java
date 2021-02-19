2cba5bb52135e97289beb2e860e327706d819ffd
migueldgoncalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2021-02-19 17:59:54 +0000

    Initial commit

---
+++ app/src/main/java/com/viajar/viajar/MainActivity.java
@@ -0,0 +1,14 @@
+package com.viajar.viajar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
