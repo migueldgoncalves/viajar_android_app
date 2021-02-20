4be08f4249eddaf29a0b9067925958fc0fbd2b09
migueldgoncalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2021-02-20 20:30:24 +0000

    Ecrã inicial criado

--- app/src/main/java/com/viajar/viajar/MainActivity.java
+++ app/src/main/java/com/viajar/viajar/MainActivity.java
@@ -3,6 +3,7 @@ package com.viajar.viajar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView version_text = findViewById(R.id.version_text);
+        String major_version = getString(R.string.version_major);
+        String minor_version = getString(R.string.version_minor);
+        String patch_version = getString(R.string.version_patch);
+        version_text.setText(getString(R.string.version_text, major_version, minor_version, patch_version));
     }
 }
