86a2755c0d45a4969a0b3db95b2a49a303ac8e1b
migueldgoncalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2021-12-31 03:03:53 +0000

    Mapa incluído na Car Activity

--- app/src/main/java/com/viajar/viajar/Car.java
+++ app/src/main/java/com/viajar/viajar/Car.java
@@ -1,9 +1,15 @@
 package com.viajar.viajar;
 
+import android.content.Context;
+
 public class Car {
 
     public static final int INPUTS_PER_SECOND = 20; // Input will be read this number of times per second
     public static final double TIME_BETWEEN_INPUTS = 1 / (double) INPUTS_PER_SECOND;
+
+    public static final int BUS = 0;
+    public static final int VAN = 1;
+    public static final int CAR = 2;
 
     // {Bus, Van, Car}
 
@@ -13,7 +19,8 @@ public class Car {
     public static final int[] MAX_SPEED = new int[]{125, 160, 250}; // km/h
     public static final int[] DECELERATION_PER_SECOND = new int[]{10, 10, 10}; // km/h lost per second when not accelerating nor braking
     public static final int[] BRAKE_PER_SECOND = new int[]{40, 40, 40}; // km/h braked per second
-    public static final int[] GEAR_NUMBER = new int[]{5, 5, 6}; // Forward gears
+    public static final int[] TOTAL_GEARS = new int[]{5, 5, 6}; // Forward gears
+    public static final int[] TOP_GEAR = new int[]{11, 11, 11}; // Supported forward gears - Last will have infinite max speed
 
     // Time for going from 0 RPM to redline in each gear in max acceleration, in seconds
     public static final int[] ACCELERATION_1ST_GEAR = new int[]{4, 4, 2};
@@ -24,18 +31,24 @@ public class Car {
     public static final int[] ACCELERATION_6TH_GEAR = new int[]{70, 60, 50};
     public static final int[] ACCELERATION_7TH_GEAR = new int[]{90, 80, 70};
     public static final int[] ACCELERATION_8TH_GEAR = new int[]{120, 105, 90};
+    public static final int[] ACCELERATION_9TH_GEAR = new int[]{150, 125, 120};
+    public static final int[] ACCELERATION_10TH_GEAR = new int[]{180, 160, 150};
+    public static final int[] ACCELERATION_11TH_GEAR = new int[]{1, 1, 1}; // Special gear
     public static final int[] ACCELERATION_REVERSE_GEAR = new int[]{4, 4, 2};
 
     // Speed at readline in km/h for each gear
-    public static final int[] SPEED_REDLINE_1ST_GEAR = new int[]{25, 35, MAX_SPEED[2] / 5};
-    public static final int[] SPEED_REDLINE_2ND_GEAR = new int[]{40, 60, 2 * (MAX_SPEED[2] / 5)};
-    public static final int[] SPEED_REDLINE_3RD_GEAR = new int[]{55, 90, 3 * (MAX_SPEED[2] / 5)};
-    public static final int[] SPEED_REDLINE_4TH_GEAR = new int[]{80, 120, 4 * (MAX_SPEED[2] / 5)};
-    public static final int[] SPEED_REDLINE_5TH_GEAR = new int[]{120, 160, 5 * (MAX_SPEED[2] / 5)};
-    public static final int[] SPEED_REDLINE_6TH_GEAR = new int[]{6 * (MAX_SPEED[2] / 5), 6 * (MAX_SPEED[2] / 5), 6 * (MAX_SPEED[2] / 5)};
-    public static final int[] SPEED_REDLINE_7TH_GEAR = new int[]{7 * (MAX_SPEED[2] / 5),7 * (MAX_SPEED[2] / 5), 7 * (MAX_SPEED[2] / 5)};
-    public static final int[] SPEED_REDLINE_8TH_GEAR = new int[]{8 * (MAX_SPEED[2] / 5),8 * (MAX_SPEED[2] / 5), 8 * (MAX_SPEED[2] / 5)};
-    public static final int[] SPEED_REDLINE_REVERSE_GEAR = new int[]{25, 35, MAX_SPEED[2] / 5};
+    public static final int[] SPEED_REDLINE_1ST_GEAR = new int[]{25, 35, MAX_SPEED[CAR] / 5};
+    public static final int[] SPEED_REDLINE_2ND_GEAR = new int[]{40, 60, 2 * (MAX_SPEED[CAR] / 5)};
+    public static final int[] SPEED_REDLINE_3RD_GEAR = new int[]{55, 90, 3 * (MAX_SPEED[CAR] / 5)};
+    public static final int[] SPEED_REDLINE_4TH_GEAR = new int[]{80, 120, 4 * (MAX_SPEED[CAR] / 5)};
+    public static final int[] SPEED_REDLINE_5TH_GEAR = new int[]{5 * (MAX_SPEED[BUS] / 5), 5 * (MAX_SPEED[VAN] / 5), 5 * (MAX_SPEED[CAR] / 5)};
+    public static final int[] SPEED_REDLINE_6TH_GEAR = new int[]{6 * (MAX_SPEED[BUS] / 5), 6 * (MAX_SPEED[VAN] / 5), 6 * (MAX_SPEED[CAR] / 5)};
+    public static final int[] SPEED_REDLINE_7TH_GEAR = new int[]{7 * (MAX_SPEED[BUS] / 5), 7 * (MAX_SPEED[VAN] / 5), 7 * (MAX_SPEED[CAR] / 5)};
+    public static final int[] SPEED_REDLINE_8TH_GEAR = new int[]{8 * (MAX_SPEED[BUS] / 5), 8 * (MAX_SPEED[VAN] / 5), 8 * (MAX_SPEED[CAR] / 5)};
+    public static final int[] SPEED_REDLINE_9TH_GEAR = new int[]{9 * (MAX_SPEED[BUS] / 5), 9 * (MAX_SPEED[VAN] / 5), 9 * (MAX_SPEED[CAR] / 5)};
+    public static final int[] SPEED_REDLINE_10TH_GEAR = new int[]{10 * (MAX_SPEED[BUS] / 5), 10 * (MAX_SPEED[VAN] / 5), 10 * (MAX_SPEED[CAR] / 5)};
+    public static final int[] SPEED_REDLINE_11TH_GEAR = new int[]{100 * (MAX_SPEED[BUS] / 5), 100 * (MAX_SPEED[VAN] / 5), 100 * (MAX_SPEED[CAR] / 5)}; // Special gear
+    public static final int[] SPEED_REDLINE_REVERSE_GEAR = new int[]{25, 35, MAX_SPEED[CAR] / 5};
 
     // Deceleration types
     public static final int DECELERATION_BRAKING = 1;
@@ -45,26 +58,15 @@ public class Car {
     private int rpm = 0;
     private int gear = 0;
     private double travelledDistance = 0.0; // km
-    private int vehiclePosition = -1;
+    private int vehicleType = -1;
     
-    public Car(String vehicle) {
-        switch (vehicle) {
-            case ("Bus"):
-            case ("Autocarro"):
-            case ("Autobús"):
-                vehiclePosition = 0;
-                break;
-            case ("Van"):
-            case ("Carrinha"):
-            case ("Camioneta"):
-                vehiclePosition = 1;
-                break;
-            case ("Car"):
-            case ("Carro"):
-            case ("Coche"):
-                vehiclePosition = 2;
-                break;
-        }
+    public Car(String vehicle, Context context) {
+        if (vehicle.equals(context.getString(R.string.vehicle_bus)))
+            vehicleType = BUS;
+        else if (vehicle.equals(context.getString(R.string.vehicle_van)))
+            vehicleType = VAN;
+        else if (vehicle.equals(context.getString(R.string.vehicle_car)))
+            vehicleType = CAR;
     }
 
     public void onClickAccelerateButton() {
@@ -88,33 +90,42 @@ public class Car {
     }
 
     private void accelerate() {
-        if (rpm < MAX_ROTATIONS[vehiclePosition]) {
+        if ((rpm < MAX_ROTATIONS[vehicleType]) || isTopGear()) { // Top gear has no speed limit
             if (gear == -1) {
-                speed -= ((double) SPEED_REDLINE_REVERSE_GEAR[vehiclePosition] / ACCELERATION_REVERSE_GEAR[vehiclePosition] / INPUTS_PER_SECOND);
+                speed -= ((double) SPEED_REDLINE_REVERSE_GEAR[vehicleType] / ACCELERATION_REVERSE_GEAR[vehicleType] / INPUTS_PER_SECOND);
             }
             else if (gear == 1) {
-                speed += ((double) SPEED_REDLINE_1ST_GEAR[vehiclePosition] / ACCELERATION_1ST_GEAR[vehiclePosition] / INPUTS_PER_SECOND);
+                speed += ((double) SPEED_REDLINE_1ST_GEAR[vehicleType] / ACCELERATION_1ST_GEAR[vehicleType] / INPUTS_PER_SECOND);
             }
             else if (gear == 2) {
-                speed += ((double) SPEED_REDLINE_2ND_GEAR[vehiclePosition] / ACCELERATION_2ND_GEAR[vehiclePosition] / INPUTS_PER_SECOND);
+                speed += ((double) SPEED_REDLINE_2ND_GEAR[vehicleType] / ACCELERATION_2ND_GEAR[vehicleType] / INPUTS_PER_SECOND);
             }
             else if (gear == 3) {
-                speed += ((double) SPEED_REDLINE_3RD_GEAR[vehiclePosition] / ACCELERATION_3RD_GEAR[vehiclePosition] / INPUTS_PER_SECOND);
+                speed += ((double) SPEED_REDLINE_3RD_GEAR[vehicleType] / ACCELERATION_3RD_GEAR[vehicleType] / INPUTS_PER_SECOND);
             }
             else if (gear == 4) {
-                speed += ((double) SPEED_REDLINE_4TH_GEAR[vehiclePosition] / ACCELERATION_4TH_GEAR[vehiclePosition] / INPUTS_PER_SECOND);
+                speed += ((double) SPEED_REDLINE_4TH_GEAR[vehicleType] / ACCELERATION_4TH_GEAR[vehicleType] / INPUTS_PER_SECOND);
             }
             else if (gear == 5) {
-                speed += ((double) SPEED_REDLINE_5TH_GEAR[vehiclePosition] / ACCELERATION_5TH_GEAR[vehiclePosition] / INPUTS_PER_SECOND);
+                speed += ((double) SPEED_REDLINE_5TH_GEAR[vehicleType] / ACCELERATION_5TH_GEAR[vehicleType] / INPUTS_PER_SECOND);
             }
             else if (gear == 6) {
-                speed += ((double) SPEED_REDLINE_6TH_GEAR[vehiclePosition] / ACCELERATION_6TH_GEAR[vehiclePosition] / INPUTS_PER_SECOND);
+                speed += ((double) SPEED_REDLINE_6TH_GEAR[vehicleType] / ACCELERATION_6TH_GEAR[vehicleType] / INPUTS_PER_SECOND);
             }
             else if (gear == 7) {
-                speed += ((double) SPEED_REDLINE_7TH_GEAR[vehiclePosition] / ACCELERATION_7TH_GEAR[vehiclePosition] / INPUTS_PER_SECOND);
+                speed += ((double) SPEED_REDLINE_7TH_GEAR[vehicleType] / ACCELERATION_7TH_GEAR[vehicleType] / INPUTS_PER_SECOND);
             }
             else if (gear == 8) {
-                speed += ((double) SPEED_REDLINE_8TH_GEAR[vehiclePosition] / ACCELERATION_8TH_GEAR[vehiclePosition] / INPUTS_PER_SECOND);
+                speed += ((double) SPEED_REDLINE_8TH_GEAR[vehicleType] / ACCELERATION_8TH_GEAR[vehicleType] / INPUTS_PER_SECOND);
+            }
+            else if (gear == 9) {
+                speed += ((double) SPEED_REDLINE_9TH_GEAR[vehicleType] / ACCELERATION_9TH_GEAR[vehicleType] / INPUTS_PER_SECOND);
+            }
+            else if (gear == 10) {
+                speed += ((double) SPEED_REDLINE_10TH_GEAR[vehicleType] / ACCELERATION_10TH_GEAR[vehicleType] / INPUTS_PER_SECOND);
+            }
+            else if (gear == 11) { // Set top gear acceleration here - Did not work when changing other gear values with very high speeds
+                speed += 10 * ((double) SPEED_REDLINE_11TH_GEAR[vehicleType] / ACCELERATION_11TH_GEAR[vehicleType] / INPUTS_PER_SECOND);
             }
         }
         upgrade_rpm();
@@ -123,10 +134,13 @@ public class Car {
     private void decelerate(int deceleration_type) {
         double speed_loss = 0;
         if (deceleration_type == DECELERATION_BRAKING) {
-            speed_loss = (double) BRAKE_PER_SECOND[vehiclePosition] / INPUTS_PER_SECOND;
+            if (isTopGear()) // Set top gear deceleration rate here
+                speed_loss = 10000 * (double) BRAKE_PER_SECOND[vehicleType] / INPUTS_PER_SECOND;
+            else
+                speed_loss = (double) BRAKE_PER_SECOND[vehicleType] / INPUTS_PER_SECOND;
         }
         else if (deceleration_type == DECELERATION_FRICTION) {
-            speed_loss = (double) DECELERATION_PER_SECOND[vehiclePosition] / INPUTS_PER_SECOND;
+            speed_loss = (double) DECELERATION_PER_SECOND[vehicleType] / INPUTS_PER_SECOND;
         }
 
         if (Math.abs(speed) > speed_loss) {
@@ -136,40 +150,51 @@ public class Car {
             else {
                 speed -= speed_loss;
             }
+        } else {
+            speed = 0;
         }
         upgrade_rpm();
     }
 
     private void upgrade_rpm() {
         if (gear == -1) {
-            rpm = (int) Math.abs(speed * REDLINE[vehiclePosition] / SPEED_REDLINE_REVERSE_GEAR[vehiclePosition]);
+            rpm = (int) Math.abs(speed * REDLINE[vehicleType] / SPEED_REDLINE_REVERSE_GEAR[vehicleType]);
         }
         else if (gear == 0) {
             rpm = 0;
         }
         else if (gear == 1) {
-            rpm = (int) (speed * REDLINE[vehiclePosition] / SPEED_REDLINE_1ST_GEAR[vehiclePosition]);
+            rpm = (int) (speed * REDLINE[vehicleType] / SPEED_REDLINE_1ST_GEAR[vehicleType]);
         }
         else if (gear == 2) {
-            rpm = (int) (speed * REDLINE[vehiclePosition] / SPEED_REDLINE_2ND_GEAR[vehiclePosition]);
+            rpm = (int) (speed * REDLINE[vehicleType] / SPEED_REDLINE_2ND_GEAR[vehicleType]);
         }
         else if (gear == 3) {
-            rpm = (int) (speed * REDLINE[vehiclePosition] / SPEED_REDLINE_3RD_GEAR[vehiclePosition]);
+            rpm = (int) (speed * REDLINE[vehicleType] / SPEED_REDLINE_3RD_GEAR[vehicleType]);
         }
         else if (gear == 4) {
-            rpm = (int) (speed * REDLINE[vehiclePosition] / SPEED_REDLINE_4TH_GEAR[vehiclePosition]);
+            rpm = (int) (speed * REDLINE[vehicleType] / SPEED_REDLINE_4TH_GEAR[vehicleType]);
         }
         else if (gear == 5) {
-            rpm = (int) (speed * REDLINE[vehiclePosition] / SPEED_REDLINE_5TH_GEAR[vehiclePosition]);
+            rpm = (int) (speed * REDLINE[vehicleType] / SPEED_REDLINE_5TH_GEAR[vehicleType]);
         }
         else if (gear == 6) {
-            rpm = (int) (speed * REDLINE[vehiclePosition] / SPEED_REDLINE_6TH_GEAR[vehiclePosition]);
+            rpm = (int) (speed * REDLINE[vehicleType] / SPEED_REDLINE_6TH_GEAR[vehicleType]);
         }
         else if (gear == 7) {
-            rpm = (int) (speed * REDLINE[vehiclePosition] / SPEED_REDLINE_7TH_GEAR[vehiclePosition]);
+            rpm = (int) (speed * REDLINE[vehicleType] / SPEED_REDLINE_7TH_GEAR[vehicleType]);
         }
         else if (gear == 8) {
-            rpm = (int) (speed * REDLINE[vehiclePosition] / SPEED_REDLINE_8TH_GEAR[vehiclePosition]);
+            rpm = (int) (speed * REDLINE[vehicleType] / SPEED_REDLINE_8TH_GEAR[vehicleType]);
+        }
+        else if (gear == 9) {
+            rpm = (int) (speed * REDLINE[vehicleType] / SPEED_REDLINE_9TH_GEAR[vehicleType]);
+        }
+        else if (gear == 10) {
+            rpm = (int) (speed * REDLINE[vehicleType] / SPEED_REDLINE_10TH_GEAR[vehicleType]);
+        }
+        else if (gear == 11) {
+            rpm = (int) (speed * REDLINE[vehicleType] / SPEED_REDLINE_11TH_GEAR[vehicleType]);
         }
     }
 
@@ -185,7 +210,7 @@ public class Car {
     }
 
     public void gearUp() {
-        if (gear < GEAR_NUMBER[vehiclePosition]) {
+        if (gear < TOP_GEAR[vehicleType]) {
             this.gear += 1;
             upgrade_rpm();
         }
@@ -234,11 +259,19 @@ public class Car {
         this.travelledDistance = travelledDistance;
     }
 
-    public int getVehiclePosition() {
-        return vehiclePosition;
+    public int getVehicleType() {
+        return vehicleType;
     }
 
-    public void setVehiclePosition(int vehiclePosition) {
-        this.vehiclePosition = vehiclePosition;
+    public void setVehicleType(int vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public boolean isGearTooHigh() { // Example: 8th gear
+        return (gear > TOTAL_GEARS[vehicleType]);
+    }
+
+    public boolean isTopGear() {
+        return (gear == TOP_GEAR[vehicleType]);
     }
 }
--- app/src/main/java/com/viajar/viajar/CarActivity.java
+++ app/src/main/java/com/viajar/viajar/CarActivity.java
@@ -8,25 +8,41 @@ import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class CarActivity extends AppCompatActivity {
+public class CarActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private Car car;
     private AtomicBoolean accelerating;
     private AtomicBoolean braking;
     private Handler mHandler;
 
+    private GoogleMap mMap;
+    private LatLng currentLocation;
+    double initialLat = 38.707658; // Lisbon
+    double initialLon = -9.136509;
+    double cameraOffset = 0.005; // Degrees - In Iberian Peninsula, 1 degree aprox. 100 km
+    double advance = 0.000005; // Degrees
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car);
 
-        car = new Car(getIntent().getStringExtra("vehicle"));
+        car = new Car(getIntent().getStringExtra("vehicle"), getApplicationContext());
         accelerating = new AtomicBoolean(false);
         braking = new AtomicBoolean(false);
         mHandler = new Handler();
@@ -36,13 +52,46 @@ public class CarActivity extends AppCompatActivity {
         findViewById(R.id.gearUpButton).setOnClickListener(onClickListener);
         findViewById(R.id.gearDownButton).setOnClickListener(onClickListener);
 
-        if (car.getVehiclePosition() != 2)
-            ((ProgressBar) findViewById(R.id.speedometer)).setMax(Car.SPEED_REDLINE_5TH_GEAR[car.getVehiclePosition()]);
+        if (car.getVehicleType() != Car.CAR)
+            ((ProgressBar) findViewById(R.id.speedometer)).setMax(Car.SPEED_REDLINE_5TH_GEAR[car.getVehicleType()]);
         else
-            ((ProgressBar) findViewById(R.id.speedometer)).setMax(Car.SPEED_REDLINE_6TH_GEAR[car.getVehiclePosition()]);
-        ((ProgressBar) findViewById(R.id.rpmMeter)).setMax(Car.MAX_ROTATIONS[car.getVehiclePosition()]);
+            ((ProgressBar) findViewById(R.id.speedometer)).setMax(Car.SPEED_REDLINE_6TH_GEAR[car.getVehicleType()]);
+        ((ProgressBar) findViewById(R.id.rpmMeter)).setMax(Car.MAX_ROTATIONS[car.getVehicleType()]);
+
+        SeekBar direction = findViewById(R.id.direction);
+        int fullCircle = 360;
+        direction.setMax(fullCircle * 2);
+        direction.setProgress(fullCircle);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapCar);
+        mapFragment.getMapAsync(this);
 
         carController.run();
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        currentLocation = new LatLng(initialLat, initialLon);
+        mMap.addMarker(new MarkerOptions().position(currentLocation).title("Current location"));
+
+        LatLngBounds.Builder b = new LatLngBounds.Builder();
+        b.include(new LatLng(initialLat + cameraOffset, initialLon - cameraOffset));
+        b.include(new LatLng(initialLat - cameraOffset, initialLon + cameraOffset));
+        LatLngBounds bounds = b.build();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 0));
     }
 
     private View.OnTouchListener onTouchListener = new View.OnTouchListener() {
@@ -85,11 +134,57 @@ public class CarActivity extends AppCompatActivity {
         ((TextView) findViewById(R.id.rpmTextView)).setText(String.valueOf(car.getRpm()));
         ((TextView) findViewById(R.id.distanceTextView)).setText(String.valueOf(car.getTravelledDistance()));
 
+        if (car.isGearTooHigh())
+            ((TextView) findViewById(R.id.gearTextView)).setTextColor(Color.RED);
+        else
+            ((TextView) findViewById(R.id.gearTextView)).setTextColor(Color.BLACK);
+
+        // Map-related code
+        if (car.getSpeed() != 0) {
+            mMap.clear();
+            int quarterCircle = 90;
+            int angle = ((SeekBar) findViewById(R.id.direction)).getProgress() - quarterCircle; // 0 - 360 -> -180 - 180
+
+            int halfCircle = 180; // Ensures starting angle points to North
+            double newLatitude = currentLocation.latitude + (Math.sin(Math.toRadians(angle + halfCircle)) * advance) * car.getSpeed();
+            int maxLatitude = 78;
+            if (newLatitude > maxLatitude) // Marker seems to not appear for low zoom levels in very high or very low latitudes
+                newLatitude = -maxLatitude;
+            else if (newLatitude < -maxLatitude)
+                newLatitude = maxLatitude;
+
+            double newLongitude = currentLocation.longitude + (Math.cos(Math.toRadians(angle + halfCircle)) * advance) * car.getSpeed();
+            int maxLongitude = 180;
+            if (newLongitude > maxLongitude)
+                newLongitude = -maxLongitude + 0.01;
+            else if (newLongitude < -maxLongitude)
+                newLongitude = maxLongitude - 0.01;
+
+            currentLocation = new LatLng(newLatitude, newLongitude);
+
+            LatLngBounds.Builder b = new LatLngBounds.Builder();
+            if (car.isTopGear()) { // Gear with infinite max speed - Countries and regions will be focused
+                int multiplier = 1000;
+                b.include(new LatLng(currentLocation.latitude + multiplier * cameraOffset, currentLocation.longitude - multiplier * cameraOffset));
+                b.include(new LatLng(currentLocation.latitude - multiplier * cameraOffset, currentLocation.longitude + multiplier * cameraOffset));
+            } else { // Standard gear - Roads will be focused
+                b.include(new LatLng(currentLocation.latitude + cameraOffset, currentLocation.longitude - cameraOffset));
+                b.include(new LatLng(currentLocation.latitude - cameraOffset, currentLocation.longitude + cameraOffset));
+            }
+            LatLngBounds bounds = b.build();
+            mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 0));
+
+            mMap.addMarker(new MarkerOptions().position(currentLocation).title("Current location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+        }
+
         String text = "";
         if (car.getGear() == -1) {
             text = (String) getText(R.string.reverse);
         } else if (car.getGear() == 0) {
             text = (String) getText(R.string.neutral);
+        } else if (car.isTopGear()) {
+            text = (String) getText(R.string.infinite);
         } else {
             text = String.valueOf(car.getGear());
         }
@@ -98,9 +193,9 @@ public class CarActivity extends AppCompatActivity {
         ((ProgressBar) findViewById(R.id.speedometer)).setProgress(car.getSpeed());
         ProgressBar rpmMeter = findViewById(R.id.rpmMeter);
         rpmMeter.setProgress(car.getRpm());
-        if (car.getRpm() < (Car.REDLINE[car.getVehiclePosition()] - Car.SUGGESTION_CHANGE_GEAR[car.getVehiclePosition()])) {
+        if (car.getRpm() < (Car.REDLINE[car.getVehicleType()] - Car.SUGGESTION_CHANGE_GEAR[car.getVehicleType()])) {
             rpmMeter.getProgressDrawable().mutate().setColorFilter(new PorterDuffColorFilter(Color.BLUE, PorterDuff.Mode.SRC_IN));
-        } else if (car.getRpm() < Car.REDLINE[car.getVehiclePosition()]) {
+        } else if (car.getRpm() < Car.REDLINE[car.getVehicleType()]) {
             rpmMeter.getProgressDrawable().mutate().setColorFilter(new PorterDuffColorFilter(Color.YELLOW, PorterDuff.Mode.SRC_IN));
         } else {
             rpmMeter.getProgressDrawable().mutate().setColorFilter(new PorterDuffColorFilter(Color.RED, PorterDuff.Mode.SRC_IN));
@@ -110,9 +205,9 @@ public class CarActivity extends AppCompatActivity {
     Runnable carController = new Runnable() {
         @Override
         public void run() {
-            if (((!accelerating.get()) && (!braking.get())) || (car.getRpm() >= Car.MAX_ROTATIONS[car.getVehiclePosition()])) {
+            if (((!accelerating.get()) && (!braking.get())) || (car.getRpm() >= Car.MAX_ROTATIONS[car.getVehicleType()])) {
                 car.decelerateByFriction();
-            } if ((accelerating.get()) && (car.getRpm() < Car.MAX_ROTATIONS[car.getVehiclePosition()])) { // Both buttons can be pressed at the same time
+            } if ((accelerating.get()) && ((car.getRpm() < Car.MAX_ROTATIONS[car.getVehicleType()]) || car.isTopGear())) { // Both buttons can be pressed at the same time
                 car.onClickAccelerateButton();
             } if (braking.get()) {
                 car.onClickBrakeButton();
